76e976f77fc9f591ce24ec3888a95d5ee5884257
Kamaz_Killer <semenvolkov780@gmail.com>
2025-12-16 09:53:40 +0300

    1

---
+++ 8.2.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("привет мир \n");
+
+
+    printf("Пырченков \n");
+    printf("Александр \n");
+    printf("Максимович \n");
+    printf("\n");
+    printf("Группа: ИСЭбд-11 \n");
+    printf("Дата: 19.09.2026 \n");
+    printf("Предмет: Основы программирования \n");
+    printf("Лабораторная работа №1 \n");
+    printf("Лабораторная работа №2 \n");
+    printf("Лабораторная работа №3 \n");
+    printf("Лабораторная работа №3 \n");
+    printf("Лабораторная работа №3 \n");
+
+    printf("Лабораторная работа №3 \n");
+
+    printf("Лабораторная работа №3 \n");
+
+    return 0;
+
+}
